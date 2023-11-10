0048ae2ea88a772ff3664b5d1ad4a0e82038196b
Charyyev Akmyrat <charyyevakmyrat17@pop-os.localdomain>
2023-11-10 18:21:55 +0500

    :lock: auth: authentication added finally

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ['gitmoji'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'auth',
+      ],
+    ],
+  },
 };
